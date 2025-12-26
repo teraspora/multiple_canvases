7a84290ee793a0475a798e6d6c7a0a3cceb9ee01
John Lynch <johnlynch.32768@gmail.com>
2025-12-26 22:20:21 +0000

    Adjust some params.

--- scripts/wacky.js
+++ scripts/wacky.js
@@ -1,30 +1,6 @@
 // Main Javascript file for Multiple Canvases
 // John Lynch - January 2024
 
-class Atom {
-    // position and velocity should be abjects with keys x and y;
-    // position values should be in the range [0, 1]; they will get multiplied by canvas dimensions in draw() method
-    // velocity is also as a proportion of canvas dimensions
-    // gravity specifies the level of attraction  to other atoms; how close they need to be, in pixels, to connect
-    static instance_count = 0;
-    constructor(radius, colour, position, velocity, gravity) {
-        this.id = Atom.instance_count++;
-        this.radius = radius;
-        this.colour = colour;
-        this.position = position;
-        this.velocity = velocity;
-        this.gravity = gravity;
-    }
-    draw(ctx) {
-        ctx.beginPath();
-        ctx.fillStyle = this.colour;
-        ctx.arc(this.position.x * ctx.canvas.width, this.position.y * ctx.canvas.height, this.radius, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.strokeStyle = '#fda';
-        ctx.stroke(); 
-    }
-}
-
 class Scene {
     static instance_count = 0;
     constructor(canvas) {
@@ -33,7 +9,7 @@ class Scene {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.progress = 0;
-        this.progress_delta = 0.02;
+        this.progress_delta = this.id * 4 + 1;
     }
     render() {
     }
@@ -50,6 +26,7 @@ class Scene2d extends Scene {
     constructor(canvas) {
         super(canvas);
         this.ctx = this.canvas.getContext("2d");
+        this.hue_initial = Math.random() * 360;
     }
     render() {
         super.render();
@@ -62,6 +39,42 @@ class Scene2d extends Scene {
         this.ctx.fillStyle = `hsl(${Math.random() * 360} 100% 50%)`;
         this.ctx.fillRect(8, 8, 16, 16);
     }
+}
+
+class StaticScene extends Scene2d {
+    constructor(canvas) {
+        super(canvas);
+        this.centre = [this.width / 2, this.height / 2];
+    }
+    render() {
+        super.render();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#18f';
+        this.ctx.lineWidth = 3;
+        this.ctx.shadowColor = "#fda";
+        this.ctx.shadowOffsetX = 2;
+        this.ctx.shadowBlur = 15;
+        this.ctx.arc(...this.centre, 64, 0.4 * Math.PI, 0.6 * Math.PI, true);
+        this.ctx.lineTo(...this.centre);
+        this.ctx.closePath();
+        this.ctx.fill();
+        this.ctx.strokeStyle = '#fda';
+        this.ctx.stroke();
+    }
+    test() {
+        // Draw something simple and small, just to verify canvas drawability!
+        this.ctx.fillStyle = `hsl(${Math.random() * 360} 100% 50%)`;
+        this.ctx.lineWidth = 4;
+        this.ctx.strokeStyle = '#2cf';
+        this.ctx.shadowColor = "#fff";
+        this.ctx.shadowOffsetX = 10;
+        this.ctx.shadowBlur = 50;
+        this.ctx.fillRect(80, 80, 160, 160);
+        this.ctx.rect(this.width - 200, this.height - 150, 50, 50);
+        this.ctx.fill();
+        this.ctx.stroke();
+    }
+
 }
 
 class VideoScene extends Scene2d {
@@ -85,7 +98,7 @@ class VideoScene extends Scene2d {
     }
 }
 
-class CurveScene extends Scene2d {            
+class CurveScene extends Scene2d {   
     // Curves
     static curves = {
         rhodonea: (k, amp, t) => [
@@ -157,7 +170,6 @@ class CurveScene extends Scene2d {
         this.params = params;
         this.x_previous = 0;
         this.y_previous = 0;
-        this.hue_initial = Math.random() * 360;
     }
 
     render() {
@@ -171,6 +183,7 @@ class CurveScene extends Scene2d {
             this.ctx.fillStyle = '#4df';
             this.ctx.shadowColor = "#fda";
             this.ctx.shadowOffsetX = 2;
+            this.ctx.shadowBlur = 15;
             this.ctx.fillText(this.curve_name, this.width - 120, this.height - 50);
             this.ctx.fillText(`(${this.params})`, this.width - 12 * this.params.toString().length - 20, this.height - 20);
         }
@@ -192,11 +205,60 @@ class CurveScene extends Scene2d {
     }
 }
 
+class Atom {
+    // position and velocity should be abjects with keys x and y;
+    // position values should be in the range [0, 1]; they will get multiplied by canvas dimensions in draw() method
+    // velocity is also as a proportion of canvas dimensions
+    // gravity specifies the level of attraction  to other atoms; how close they need to be, in pixels, to connect
+    static instance_count = 0;
+    constructor(radius, colour, position, velocity, gravity, trail_length) {
+        this.id = Atom.instance_count++;
+        this.radius = radius;
+        this.colour = colour;
+        this.position = position;
+        this.velocity = velocity;
+        this.gravity = gravity;
+        this.trail_length = trail_length;
+        this.trailpoints = [[this.position.x, this.position.y]];
+        this.hue_initial = Math.random() * 360;
+    }
+    draw(ctx, scene_id) {
+        if (scene_id != 1) {    // For Scene 1, don't draw the actual atoms!
+            ctx.beginPath();
+            ctx.fillStyle = this.colour;
+            // debugger;
+            const canvas_x = this.position.x * ctx.canvas.width;
+            const canvas_y = this.position.y * ctx.canvas.height;
+            ctx.arc(canvas_x, canvas_y, this.radius, 0.3 * Math.PI, 0.7 * Math.PI, true);
+            ctx.lineTo(canvas_x, canvas_y);
+            ctx.closePath();
+            ctx.fill();
+        }
+        // Draw trails
+        ctx.beginPath();
+        ctx.moveTo(this.trailpoints[0][0] * ctx.canvas.width, this.trailpoints[0][1] * ctx.canvas.height);
+        ctx.strokeStyle = `hsl(${Math.sin(this.hue_initial + (this.velocity.x + this.velocity.y) * 32767) * 90 + 270} 100% 50%)`;
+        for (const point of this.trailpoints.slice(1)) {
+            ctx.lineTo(point[0] * ctx.canvas.width, point[1] * ctx.canvas.height);
+        }
+        ctx.stroke();
+        this.trailpoints.push([this.position.x, this.position.y]);
+        if (this.trailpoints.length > this.trail_length) {
+            this.trailpoints.shift();
+        }
+    }
+}
+
 class AtomScene extends Scene2d {
     constructor(canvas, atoms) {
         super(canvas);
         this.atoms = atoms;
-        this.colour_connections = Math.random() < 0.7;
+        this.colour_connections = this.id % 2;
+        this.wiggly_trails = this.id == 2 || this.id == 1;
+        // Following is very bad on performance!
+        // this.ctx.shadowColor = "#fda";
+        // this.ctx.shadowOffsetX = 2;
+        // this.ctx.shadowBlur = 15;
     }
 
     render() {
@@ -210,14 +272,24 @@ class AtomScene extends Scene2d {
     }
 
     update() {
+        // debugger;
         // Called for every repaint, by requestAnimationFrame()
         super.update();
         // Here, update all the atom positions and draw them
         this.ctx.clearRect(0, 0, this.width, this.height);
         for (const atom of this.atoms) {
             // Set new position for each atom
-            atom.position.x += atom.velocity.x;
-            atom.position.y += atom.velocity.y;
+            atom.position.x += atom.velocity.x / Math.sin(this.progress * 10) * 11;
+            atom.position.y += atom.velocity.y / Math.cos(this.progress * 10) * 13;
+            if (this.wiggly_trails) {
+                atom.position.x += Math.random() * 0.02 - 0.01;
+                atom.position.y += Math.random() * 0.02 - 0.01;
+            }
+            else if (!this.id) {
+                // atom.position.x += Math.sin(this.progress) * 0.000000000001;
+                // atom.position.y += Math.sin(this.progress) * 0.000000000001;
+            }
+
             // Check if atom is going offscreen, and if so, bounce it back
             if (atom.position.x * this.width <= atom.radius && atom.velocity.x < 0) {
                 atom.position.x = atom.radius / this.width;
@@ -239,30 +311,31 @@ class AtomScene extends Scene2d {
                 atom.velocity.y = -atom.velocity.y;
             }
         }  
-        
-        // Now check the proximity of each pair of atoms, and connect them if they are closer than the lesser of the two gravities
-        for (let [atom_a, atom_b] of this.atoms.map((atom_a, i) => this.atoms.slice(i + 1).map(atom_b => [atom_a, atom_b])).flat()) {
-            const proximity = Math.hypot((atom_b.position.x - atom_a.position.x) * this.width, (atom_b.position.y - atom_a.position.y) * this.height);
-            if (proximity < Math.min(atom_a.gravity, atom_b.gravity)) {
-                this.ctx.lineWidth = 1;
-                this.ctx.strokeStyle = this.colour_connections
-                    ? `lch(50% 132 ${rand_int(360)} / ${Math.floor(100 - Math.min(proximity, 100))}%)`
-                    : `rgb(255 255 255 / ${Math.floor(100 - Math.min(proximity, 100))}%)`;
-                this.ctx.beginPath();
-                this.ctx.moveTo(atom_a.position.x * this.width, atom_a.position.y * this.height);
-                this.ctx.lineTo(atom_b.position.x * this.width, atom_b.position.y * this.height);
-                this.ctx.stroke();
-                if (proximity <= atom_a.radius + atom_b.radius) {
-                    atom_a.velocity.x *= -1;
-                    atom_a.velocity.y *= -1;
-                    atom_b.velocity.x *= -1;
-                    atom_b.velocity.y *= -1;
+        if (this.id > 1) {
+            // Now check the proximity of each pair of atoms, and connect them if they are closer than the lesser of the two gravities
+            for (let [atom_a, atom_b] of this.atoms.map((atom_a, i) => this.atoms.slice(i + 1).map(atom_b => [atom_a, atom_b])).flat()) {
+                const proximity = Math.hypot((atom_b.position.x - atom_a.position.x) * this.width, (atom_b.position.y - atom_a.position.y) * this.height);
+                if (proximity < Math.min(atom_a.gravity, atom_b.gravity)) {
+                    this.ctx.lineWidth = 1;
+                    this.ctx.strokeStyle = this.colour_connections
+                        ? `lch(50% 132 ${rand_int(360)} / ${Math.floor(100 - Math.min(proximity, 100))}%)`
+                        : `rgb(255 255 255 / ${Math.floor(100 - Math.min(proximity, 100))}%)`;
+                    this.ctx.beginPath();
+                    this.ctx.moveTo(atom_a.position.x * this.width, atom_a.position.y * this.height);
+                    this.ctx.lineTo(atom_b.position.x * this.width, atom_b.position.y * this.height);
+                    this.ctx.stroke();
+                    if (proximity <= atom_a.radius + atom_b.radius) {
+                        atom_a.velocity.x *= -1;
+                        atom_a.velocity.y *= -1;
+                        atom_b.velocity.x *= -1;
+                        atom_b.velocity.y *= -1;
+                    }
                 }
             }
         }
         // Finally, draw the atoms themselves      
         for (let atom of this.atoms) {
-            atom.draw(this.ctx);
+            atom.draw(this.ctx, this.id);   // pass the scene id so we can decide what to draw based on that.
         }
 
         requestAnimationFrame(this.update.bind(this));
@@ -270,9 +343,6 @@ class AtomScene extends Scene2d {
 }
 
 function init() {
-    if (debug) {
-        canvas_count = 4;
-    }
     const scenes = [];
     const cols = Math.sqrt(canvas_count);
     const main = document.getElementById('main');
@@ -293,16 +363,16 @@ function init() {
     });
 
     // For each canvas, create a new Scene, and push the new Scene to an array of scenes.
-    let curve, params, scene;
+    let curve, params;
     canvases.forEach((canvas, index) => {
-        if (Math.random() > 0.8) {
-            canvas.style.filter = filters[rand_int(filters.length)];
-        }
-        let i = rand_int(24);
-        if (i < 14 && !debug) {
+        // if (Math.random() > 0.8) {
+        //     canvas.style.filter = filters[rand_int(filters.length)];
+        // }
+        let s;  // will be a Scene of some subtype
+        if (index < -14) {
             // Create a Curve Scene
             let amp, k, a, b, c, r, density, x_wobble_amp, y_wobble_amp, x_wobble_freq, y_wobble_freq;
-            switch(i) {
+            switch(index) {
                 case 0:
                     curve = 'hcrr';
                     params = [Math.random() * 10, Math.random() * 4, rand_in_range(16, 32)];
@@ -339,8 +409,8 @@ function init() {
                     params = [k, amp];
                     break;
                 case 4:
-                    rand_in_range(10, canvas.width / 2);
-                    rand_in_range(2, canvas.width / 4);
+                    a = rand_in_range(10, canvas.width / 2);
+                    b = rand_in_range(2, canvas.width / 4);
                     amp = Math.random() + 0.2;
                     curve = 'hypocycloid';
                     params = [a, b, amp] ;
@@ -360,50 +430,43 @@ function init() {
                     break;
                 }
 
-            scene = new CurveScene(canvas, curve, params, canvas_count < 5 ? 1 : rand_in_range(1, 3));
-            scenes.push(scene);
+            s = new CurveScene(canvas, curve, params, canvas_count < 5 ? 1 : rand_in_range(1, 3));
+            scenes.push(s);
         }
-        else if (i < 20 || debug) {
+        else if (true) {
             // Create an Atom Scene
             let atoms = [];
             const pixel_count = canvas.width * canvas.height;
-            let atom_count = rand_in_range(pixel_count / 8192, pixel_count / 4096);
-            // if (canvases.length == 1) {
-            //     atom_count *= 3;
-            // }
+            // let atom_count = rand_in_range(pixel_count / 8192, pixel_count / 4096);
+            let atom_count = 128;
             for (let i = 0; i < atom_count; i++) {
-                const radius = rand_in_range(2,11);
+                const radius = rand_in_range(2,6);
                 const colour = `lch(50% 132 ${rand_int(360)})`;
                 const position = {x: Math.random(), y: Math.random()};
-                const velocity = {x: Math.random() * 0.006 - 0.003, y: Math.random() * 0.006 - 0.003};
+                const velocity = {x: Math.random() * 0.001 - 0.0005, y: Math.random() * 0.001 - 0.0005};
                 const gravity = canvas.width / 2;
+                const trail_length = index == 0 || index == 3 ? 96 : 2048;
                 atoms.push(new Atom(
                     radius,
                     colour,
                     position,
                     velocity,
-                    gravity
+                    gravity,
+                    trail_length
                 ));
             }
-            scene = new AtomScene(canvas, atoms);
-            scenes.push(scene);
+            s = new AtomScene(canvas, atoms);
+            scenes.push(s);
         }
         else {
-            // Create a video scene
-            if (!available_videos.length) {
-                available_videos = all_videos;    // used them up, so have to reuse them!
-            }
-            const index = rand_int(available_videos.length);
-            const video = available_videos[index];
-            // Remove the current video from the available list
-            available_videos = available_videos.filter(v => v != video);
-            scene = new VideoScene(canvas, video);
-            scenes.push(scene);
+            // Create a static scene
+            s = new StaticScene(canvas);
+            scenes.push(s);
         }
 
-        if (debug) {
+        if (DEBUG) {
             // test() method should draw something simple and small, just to verify canvas drawability!
-            scene.test();
+            s.test();
         }
     });
     
@@ -414,15 +477,12 @@ function init() {
 }
 
 // Top-level code
-let debug = false;
-const default_canvas_count = 16; // must be a perfect square!
-const help = document.querySelector('aside#help');
+const DEBUG = true;
 const rand_int = n => Math.floor(n * Math.random());
 const rand_in_range = (m, n) => Math.floor((n - m) * Math.random() + m);
-let canvas_count = default_canvas_count;
-let show_curve_info = false;
+let canvas_count = 4; // must be a perfect square!
+let show_curve_info = true;
 const toggle_curve_info = _ => show_curve_info = !show_curve_info;
-const toggle_debug = _ => debug = !debug;
 const filters = ['none', 'hue-rotate(45deg)', 'sepia(1)', 'invert(1)'];
 
 // Allow user to hit a digit key to refresh with a different number of canvases -
@@ -444,24 +504,10 @@ window.addEventListener('keyup', event => {
                     toggle_curve_info();
                     init()
                     break;
-                case 'd':
-                    // 'c' is standard for toggling subtitles on Youtube, so...
-                    toggle_debug();
-                    canvas_count = default_canvas_count;
-                    init();
-                    break;
-                case 'h':
-                    // 
-                    help.style.display = 'block';
-                    break;
                 default:
             }
         }
     }
-});
-
-document.getElementById('close-help').addEventListener('click', _ => {
-    help.style.display = 'none';
 });
 
 ['load', 'resize'].forEach(event => window.addEventListener(event, init));
